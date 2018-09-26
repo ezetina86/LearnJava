31c372666187d2514d1ee7af9554acf67e197fb9
ezetina86 <jenzetin@gmail.com>
2018-09-26 12:29:40 -0500

    Clase 92

---
+++ Ventas/src/com/gm/ventas/Orden.java
@@ -0,0 +1,42 @@
+package com.gm.ventas;
+
+public class Orden {
+	
+	private final int idOrden; 
+	private final Producto productos[];
+	private static int contadorOrdenes;
+	private int contadorProductos;
+	private static final int maxProductos=10;
+	
+	public Orden() {
+		this.idOrden = ++contadorOrdenes;
+		productos = new Producto[maxProductos];
+	}
+	
+	public void agregarProducto(Producto producto) {
+		if(contadorProductos <  maxProductos) {
+			productos[contadorProductos++]= producto;
+		}
+		else {
+			System.out.println("Se ha superado el maximo de productos: " + maxProductos);
+		}
+	}
+	
+	public double calcularTotal() {
+		double total = 0;
+		for (int i=0; i < contadorProductos; i++) {
+			total += productos[i].getPrecio();
+		}
+		return total;
+	}
+	
+	public void mostrarOrden() {
+		System.out.println("Orden #" + idOrden);
+		System.out.println("Total de la Orden #" + idOrden + " $" + calcularTotal());
+		System.out.println("Productos de la Orden #" + idOrden + ": " );
+		for(int i=0; i< contadorProductos; i++) {
+			System.out.println(productos[i]);
+		}
+	}
+
+}
---
+++ Ventas/src/ventas/Ventas.java
@@ -0,0 +1,39 @@
+package ventas;
+
+
+import com.gm.ventas.*;
+
+
+public class Ventas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Producto p1 = new Producto("Camisa",50.00);
+		Producto p2 = new Producto("Pantalon",100.00);
+		Producto p3 = new Producto("Corbata",30.00);
+		
+		Orden orden1 = new Orden();
+		
+		orden1.agregarProducto(p1);
+		orden1.agregarProducto(p2);
+		orden1.agregarProducto(p3);
+		
+		orden1.mostrarOrden();
+		
+		Orden orden2= new Orden();
+		
+		Producto p4 = new Producto("Zapatos",200);
+		Producto p5 = new Producto("Blusa",90);
+		
+		orden2.agregarProducto(p1);
+		orden2.agregarProducto(p4);
+		orden2.agregarProducto(p5);
+		orden2.agregarProducto(p3);
+		
+		System.out.println("");
+		orden2.mostrarOrden();
+		
+	}
+
+}
