55fdc40aae5db7911f99d15643a4bf8164c1967a
ezetina86 <jenzetin@gmail.com>
2019-01-23 19:40:11 -0600

    Adding lombok.jar to Platzi project in order to manage  @Data with Spring & Maven

---
+++ Spring/e-reservation/src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package com.platzi.ereservation.modelo;
+
+import lombok.Data;
+
+/***
+ * Clase que representa la tabla cliente
+ * @author Henry
+ * 
+ */
+@Data
+public class Cliente {
+	 String nombreCli;
+	 String apellidoCli;
+	
+	public Cliente() {	
+	
+	}
+}
