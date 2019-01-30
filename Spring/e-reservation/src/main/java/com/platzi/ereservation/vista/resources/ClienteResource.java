7d10aa76729024daa09302ed7aaaa858fcb7e64c
ezetina86 <jenzetin@gmail.com>
2019-01-30 10:39:56 -0600

    Adding Spring security

--- Spring/e-reservation/src/main/java/com/platzi/ereservation/vista/resources/ClienteResource.java
+++ Spring/e-reservation/src/main/java/com/platzi/ereservation/vista/resources/ClienteResource.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
-import org.hibernate.annotations.GenericGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
